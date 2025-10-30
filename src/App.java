da11ed46835eb48c3b89df7c072c1328dcc896cb
Curro Portillo <211curroportillo@gmail.com>
2025-10-30 19:16:04 +0100

    ejercicio 25

--- src/App.java
+++ src/App.java
@@ -1,5 +1,14 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero = Integer.parseInt(System.console().readLine("Introduzca un número entero: "));
+        int aux = numero;
+        int invertido = 0;
+        int digito = 0;
+        do{
+            digito = aux % 10;
+            invertido = (invertido * 10) + digito;
+            aux /= 10;
+        }while(aux != 0);
+        System.out.printf("Si le damos la vuelta al %d tenemos el %d.", numero, invertido);
     }
 }
